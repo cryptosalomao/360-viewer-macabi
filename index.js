a792a9ff8bc66f323cb268b8d40a1da82c3c20ce
cryptosalomao <cryptosalomao@gmail.com>
2020-06-25 13:25:10 -0300

    Update bundle

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const container = document.getElementById('viewer');
 
 const viewer = new PhotoSphereViewer({
   container,
-  panorama: './img/13-andar.jpg',
+  panorama: './img/6-andar.jpg',
   navbar: [
     'autorotate',
     'zoom',
